31e8c3d6a854decabeb9e77d223558a4751ad860
fffzlfk <1319933925qq@gmail.com>
2022-01-16 22:01:39 +0800

    Add some boring code

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,10 @@
 // #include <basic/erosion_dilatation.hpp>
 // #include <basic/opening_closing_hats.hpp>
 // #include <basic/hit_or_miss.hpp>
-#include <basic/morph_lines_detection.hpp>
+// #include <basic/morph_lines_detection.hpp>
+// #include <transformations/filter_2d.hpp>
+// #include <transformations/copy_make_border.hpp>
+#include <transformations/sobel_derivatives.hpp>
 
 int main(int argc, char *argv[]) {
     // basic::basic_drawing::work();
@@ -12,6 +15,9 @@ int main(int argc, char *argv[]) {
     // basic::blur::work(argc, argv);
     // basic::opening_closing_hats::work(argc, argv);
     // basic::hit_or_miss::work();
-    basic::morph_lines_detection::work(argc, argv);
+    // basic::morph_lines_detection::work(argc, argv);
+    // transformations::filter_2d::work(argc, argv);
+    // transformations::copy_make_border::work(argc, argv);
+    transformations::sobel_derivatives::work(argc, argv);
     return 0;
 }
---
+++ src/transformations/include/transformations/sobel_derivatives.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/imgproc.hpp"
+
+#include <iostream>
+
+namespace transformations {
+namespace sobel_derivatives {
+
+using namespace cv;
+using namespace std;
+int work(int argc, char *argv[]);
+} // namespace sobel_derivatives
+} // namespace transformations
